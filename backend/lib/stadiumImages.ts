681846572916b36063d1402eed703928e0cb74e3
Kush Shukla <Kushpearl456@gmail.com>
2026-01-25 04:03:46 +0530

    Fix stadiumImages formatting

--- backend/lib/stadiumImages.ts
+++ backend/lib/stadiumImages.ts
@@ -1,1 +1,9 @@
-export const stadiumImages = ["1769289232190.png","1769289428514-Photoroom.png","1769289436056-Photoroom.png","1769289562412.png","1769289580658-Photoroom.png","1769289876831-Photoroom.png","1769289969991-Photoroom.png"] as const;\n
+export const stadiumImages = [
+  "1769289232190.png",
+  "1769289428514-Photoroom.png",
+  "1769289436056-Photoroom.png",
+  "1769289562412.png",
+  "1769289580658-Photoroom.png",
+  "1769289876831-Photoroom.png",
+  "1769289969991-Photoroom.png"
+] as const;
